9fc02242a3e2fe87f02fb3486056d2608425fcd8
Andrade Sampaio <andrade.sampaio@outlook.com>
2019-02-21 12:32:44 -0300

    Correção da Classe Quote erro nos Atributos

--- src/main/java/challenge/Quote.java
+++ src/main/java/challenge/Quote.java
@@ -17,30 +17,37 @@ public class Quote {
 	@Column(name="detail")
 	private String quote;
 	
+	
 	public Integer getId() {
-		return null;
+		return id;
 	}
+
 
 	public void setId(Integer id) {
-
+		this.id = id;
 	}
+
 
 	public String getActor() {
-		return null;
+		return actor;
 	}
+
 
 	public void setActor(String actor) {
-
+		this.actor = actor;
 	}
+
 
 	public String getQuote() {
-		return null;
+		return quote;
 	}
+
 
 	public void setQuote(String quote) {
-
+		this.quote = quote;
 	}
-	
+
+
 	@Override
 	public String toString() {
 		return "Quote [id=" + id + ", actor=" + actor + ", quote=" + quote + "]";
